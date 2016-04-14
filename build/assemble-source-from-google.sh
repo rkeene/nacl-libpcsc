ce626024669599decae556fd61b6aa48912aa169
rkeene <rkeene@noemail.net>
2016-04-14 07:21:36 +0000

    Try to assemble a working JavaScript library
    FossilOrigin-Name: 7ce79d6ede3de3488da838965c9d407ba3113eea

--- build/assemble-source-from-google.sh
+++ build/assemble-source-from-google.sh
@@ -37,9 +37,108 @@ function assemblePCSC() {
 		cp third_party/pcsc-lite/src-*/src/PCSC/*.h "${instdir}/include/PCSC" || exit 1
 
 		# Copy out JavaScript files for later use
-#		cp common-utils/*.js "${instdir}/js" || exit 1
-#		cp third_party/pcsc-lite/client-side/*.js "${instdir}/js" || exit 1
-		touch "${instdir}/libpcsc.js"
+		rm -f "${instdir}/libpcsc.js"
+		files=(
+			third_party/closure-library/src-20160208/closure/goog/base.js
+			third_party/closure-library/src-20160208/closure/goog/math/integer.js
+			common/js/src/fixed-size-integer.js
+			third_party/closure-library/src-20160208/closure/goog/debug/error.js
+			third_party/closure-library/src-20160208/closure/goog/dom/nodetype.js
+			third_party/closure-library/src-20160208/closure/goog/string/string.js
+			third_party/closure-library/src-20160208/closure/goog/asserts/asserts.js
+			third_party/closure-library/src-20160208/closure/goog/array/array.js
+			third_party/closure-library/src-20160208/closure/goog/functions/functions.js
+			third_party/closure-library/src-20160208/closure/goog/math/math.js
+			third_party/closure-library/src-20160208/closure/goog/iter/iter.js
+			third_party/closure-library/src-20160208/closure/goog/json/json.js
+			third_party/closure-library/src-20160208/closure/goog/object/object.js
+			third_party/closure-library/src-20160208/closure/goog/structs/structs.js
+			third_party/closure-library/src-20160208/closure/goog/structs/map.js
+			common/js/src/logging/debug-dump.js
+			third_party/closure-library/src-20160208/closure/goog/dom/tagname.js
+			third_party/closure-library/src-20160208/closure/goog/dom/tags.js
+			third_party/closure-library/src-20160208/closure/goog/string/typedstring.js
+			third_party/closure-library/src-20160208/closure/goog/string/const.js
+			third_party/closure-library/src-20160208/closure/goog/html/safestyle.js
+			third_party/closure-library/src-20160208/closure/goog/html/safestylesheet.js
+			third_party/closure-library/src-20160208/closure/goog/fs/url.js
+			third_party/closure-library/src-20160208/closure/goog/i18n/bidi.js
+			third_party/closure-library/src-20160208/closure/goog/html/safeurl.js
+			third_party/closure-library/src-20160208/closure/goog/html/trustedresourceurl.js
+			third_party/closure-library/src-20160208/closure/goog/labs/useragent/util.js
+			third_party/closure-library/src-20160208/closure/goog/labs/useragent/browser.js
+			third_party/closure-library/src-20160208/closure/goog/html/safehtml.js
+			third_party/closure-library/src-20160208/closure/goog/html/safescript.js
+			third_party/closure-library/src-20160208/closure/goog/html/uncheckedconversions.js
+			third_party/closure-library/src-20160208/closure/goog/structs/collection.js
+			third_party/closure-library/src-20160208/closure/goog/structs/set.js
+			third_party/closure-library/src-20160208/closure/goog/labs/useragent/engine.js
+			third_party/closure-library/src-20160208/closure/goog/labs/useragent/platform.js
+			third_party/closure-library/src-20160208/closure/goog/useragent/useragent.js
+			third_party/closure-library/src-20160208/closure/goog/debug/debug.js
+			third_party/closure-library/src-20160208/closure/goog/debug/logrecord.js
+			third_party/closure-library/src-20160208/closure/goog/debug/logbuffer.js
+			third_party/closure-library/src-20160208/closure/goog/debug/logger.js
+			third_party/closure-library/src-20160208/closure/goog/debug/relativetimeprovider.js
+			third_party/closure-library/src-20160208/closure/goog/debug/formatter.js
+			third_party/closure-library/src-20160208/closure/goog/log/log.js
+			third_party/closure-library/src-20160208/closure/goog/structs/circularbuffer.js
+			common/js/src/logging/log-buffer.js
+			third_party/closure-library/src-20160208/closure/goog/debug/console.js
+			common/js/src/logging/logging.js
+			common/js/src/random.js
+			third_party/closure-library/src-20160208/closure/goog/disposable/idisposable.js
+			third_party/closure-library/src-20160208/closure/goog/disposable/disposable.js
+			third_party/closure-library/src-20160208/closure/goog/promise/thenable.js
+			third_party/closure-library/src-20160208/closure/goog/async/freelist.js
+			third_party/closure-library/src-20160208/closure/goog/async/workqueue.js
+			third_party/closure-library/src-20160208/closure/goog/debug/entrypointregistry.js
+			third_party/closure-library/src-20160208/closure/goog/async/nexttick.js
+			third_party/closure-library/src-20160208/closure/goog/async/run.js
+			third_party/closure-library/src-20160208/closure/goog/promise/resolver.js
+			third_party/closure-library/src-20160208/closure/goog/promise/promise.js
+			third_party/closure-library/src-20160208/closure/goog/events/browserfeature.js
+			third_party/closure-library/src-20160208/closure/goog/events/eventid.js
+			third_party/closure-library/src-20160208/closure/goog/events/event.js
+			third_party/closure-library/src-20160208/closure/goog/events/eventtype.js
+			third_party/closure-library/src-20160208/closure/goog/reflect/reflect.js
+			third_party/closure-library/src-20160208/closure/goog/events/browserevent.js
+			third_party/closure-library/src-20160208/closure/goog/events/listenable.js
+			third_party/closure-library/src-20160208/closure/goog/events/listener.js
+			third_party/closure-library/src-20160208/closure/goog/events/listenermap.js
+			third_party/closure-library/src-20160208/closure/goog/events/events.js
+			third_party/closure-library/src-20160208/closure/goog/events/eventtarget.js
+			third_party/closure-library/src-20160208/closure/goog/timer/timer.js
+			third_party/closure-library/src-20160208/closure/goog/messaging/messagechannel.js
+			third_party/closure-library/src-20160208/closure/goog/messaging/abstractchannel.js
+			common/js/src/messaging/message-channel-pinging.js
+			common/js/src/messaging/typed-message.js
+			common/js/src/messaging/port-message-channel.js
+			common/js/src/nacl-module/nacl-module-log-messages-receiver.js
+			common/js/src/nacl-module/nacl-module-messaging-channel.js
+			third_party/closure-library/src-20160208/closure/goog/dom/browserfeature.js
+			third_party/closure-library/src-20160208/closure/goog/dom/safe.js
+			third_party/closure-library/src-20160208/closure/goog/math/coordinate.js
+			third_party/closure-library/src-20160208/closure/goog/math/size.js
+			third_party/closure-library/src-20160208/closure/goog/dom/dom.js
+			common/js/src/nacl-module/nacl-module.js
+			common/js/src/requesting/remote-call-message.js
+			common/js/src/requesting/request-handler.js
+			common/js/src/requesting/requester-message.js
+			common/js/src/requesting/request-receiver.js
+			common/js/src/requesting/requester.js
+			third_party/closure-library/src-20160208/closure/goog/structs/queue.js
+			third_party/pcsc-lite/naclport/common/src/constants.js
+			third_party/pcsc-lite/naclport/js_client/src/api.js
+			third_party/pcsc-lite/naclport/js_client/src/context.js
+			third_party/pcsc-lite/naclport/cpp_client/src/nacl-client-request-handler.js
+			third_party/pcsc-lite/naclport/cpp_client/src/nacl-client-backend.js
+		)
+		cat "${files[@]}" | sed '
+			/^ *goog\.require(/ d;
+			s@^goog\.DEPENDENCIES_ENABLED = .*$@goog.DEPENDENCIES_ENABLED = false;@
+		' > "${instdir}/libpcsc.js.new" || exit 1
+		mv "${instdir}/libpcsc.js.new" "${isntdir}/libpcsc.js" || exit 1
 
 		# Assemble all the files into a single tree
 		files=(
